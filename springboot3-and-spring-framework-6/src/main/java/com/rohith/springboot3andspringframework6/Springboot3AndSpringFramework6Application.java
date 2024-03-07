f098a55ae7485de827c07b8757014b55e740328e
rohithreddybc <rohithreddybc98@gmail.com>
2024-03-07 10:55:51 -0500

    Creating springboot3andspringframework6 Project

---
+++ springboot3-and-spring-framework-6/src/main/java/com/rohith/springboot3andspringframework6/Springboot3AndSpringFramework6Application.java
@@ -0,0 +1,13 @@
+package com.rohith.springboot3andspringframework6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3AndSpringFramework6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3AndSpringFramework6Application.class, args);
+	}
+
+}
