4bdf1ebc82f13c3ae73983f5e2aea134c8345e52
rohithreddybc <rohithreddybc98@gmail.com>
2024-03-13 23:52:49 -0400

    App02HelloWorldSpring

---
+++ springboot3-and-spring-framework-6/src/main/java/com/rohith/springboot3andspringframework6/App02HelloWorldSpring.java
@@ -0,0 +1,15 @@
+package com.rohith.springboot3andspringframework6;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+	}
+
+}
